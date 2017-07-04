b18da7954c112feef74cc7170536e3e4706d8b77
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-07-04 09:24:24 +0200

    Released version 1.18
     - Fix rendering of documentation does not die when "parameters" are under a path
     - Fix generating routes with "parameters" under a path #42
     - Fix other documentation renderers, when "parameters" under a pth #42

--- lib/Mojolicious/Plugin/OpenAPI.pm
+++ lib/Mojolicious/Plugin/OpenAPI.pm
@@ -6,7 +6,7 @@ use Mojo::JSON;
 use Mojo::Util 'deprecated';
 use constant DEBUG => $ENV{MOJO_OPENAPI_DEBUG} || 0;
 
-our $VERSION = '1.17';
+our $VERSION = '1.18';
 my $X_RE = qr{^x-};
 
 has _validator => sub { JSON::Validator::OpenAPI::Mojolicious->new; };
