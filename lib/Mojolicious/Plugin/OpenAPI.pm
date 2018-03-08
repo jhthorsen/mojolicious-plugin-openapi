fa24878bdfdb297d06bf92f3a86493e21fc3583e
Jan Henning Thorsen <jhthorsen@cpan.org>
2018-03-08 21:16:14 +0100

    Released version 1.26
     - Fix skipping yaml.t, unless correct version of YAML::XS is available #67
       Contributor: Søren Lund

--- lib/Mojolicious/Plugin/OpenAPI.pm
+++ lib/Mojolicious/Plugin/OpenAPI.pm
@@ -6,7 +6,7 @@ use Mojo::JSON;
 use Mojo::Util 'deprecated';
 use constant DEBUG => $ENV{MOJO_OPENAPI_DEBUG} || 0;
 
-our $VERSION = '1.25';
+our $VERSION = '1.26';
 my $X_RE = qr{^x-};
 
 has route     => sub {undef};
