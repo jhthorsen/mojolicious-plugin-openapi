dcf88e279b4e4330e49ec265c9653b3da41ff0a9
Jan Henning Thorsen <jhthorsen@cpan.org>
2018-11-14 15:42:30 +0900

    Released version 2.03
     - Improved human readable documentation rendering

--- lib/Mojolicious/Plugin/OpenAPI.pm
+++ lib/Mojolicious/Plugin/OpenAPI.pm
@@ -12,7 +12,7 @@ Mojo::Util::monkey_patch(__PACKAGE__,
   ? sub { Mojo::ByteStream->new(Text::Markdown::markdown($_[0])) }
   : sub { $_[0] });
 
-our $VERSION = '2.02';
+our $VERSION = '2.03';
 my $X_RE = qr{^x-};
 
 has route     => sub {undef};
