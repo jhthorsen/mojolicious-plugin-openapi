b9bf9e1f6c20207706abad6301cc6f55c15df61b
Jan Henning Thorsen <jhthorsen@cpan.org>
2018-11-15 16:14:10 +0900

    Released version 2.04
     - Use data:///file.json in SYNOPSIS to make it work with morbo

--- lib/Mojolicious/Plugin/OpenAPI.pm
+++ lib/Mojolicious/Plugin/OpenAPI.pm
@@ -12,7 +12,7 @@ Mojo::Util::monkey_patch(__PACKAGE__,
   ? sub { Mojo::ByteStream->new(Text::Markdown::markdown($_[0])) }
   : sub { $_[0] });
 
-our $VERSION = '2.03';
+our $VERSION = '2.04';
 my $X_RE = qr{^x-};
 
 has route     => sub {undef};
