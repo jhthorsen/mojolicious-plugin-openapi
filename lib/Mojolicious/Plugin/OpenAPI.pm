5729e1b068845ce10553e2cdb361fe508f8cc76f
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-01-26 11:37:19 +0900

    Released version 2.11
     - Fix allowing regular requests with "openapi_cors_allowed_origins" #103

--- lib/Mojolicious/Plugin/OpenAPI.pm
+++ lib/Mojolicious/Plugin/OpenAPI.pm
@@ -7,7 +7,7 @@ use Mojo::JSON;
 use Mojo::Util;
 use constant DEBUG => $ENV{MOJO_OPENAPI_DEBUG} || 0;
 
-our $VERSION = '2.10';
+our $VERSION = '2.11';
 my $X_RE = qr{^x-};
 
 has route     => sub {undef};
