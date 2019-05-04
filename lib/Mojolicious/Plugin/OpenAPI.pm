630125c56be1f5173d2f603085727d0fc5511ae2
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-05-04 21:23:41 +0700

    Fix coerce(1) will be deprecated #130

--- lib/Mojolicious/Plugin/OpenAPI.pm
+++ lib/Mojolicious/Plugin/OpenAPI.pm
@@ -26,7 +26,7 @@ has _renderer => sub {
 sub register {
   my ($self, $app, $config) = @_;
 
-  $self->validator->coerce($config->{coerce} // 1);
+  $self->validator->coerce($config->{coerce} // 'booleans,numbers,strings');
   $self->validator->load_and_validate_schema(
     $config->{url} || $config->{spec},
     {
@@ -525,7 +525,9 @@ Note that setting this attribute is discourage.
 
 See L<JSON::Validator/coerce> for possible values that C<coerce> can take.
 
-Default: 1
+Default: booleans,numbers,strings
+
+The default value will include "defaults" in the future, once that is stable enough.
 
 =head3 default_response_codes
 
