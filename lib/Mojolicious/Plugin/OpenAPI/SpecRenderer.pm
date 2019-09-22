62e240114debd7255a2f539099107c4df6e5b06e
Henrik Andersen <hem@hamster.dk>
2019-09-22 10:52:17 +0200

    this object belongs under components->schemas

--- lib/Mojolicious/Plugin/OpenAPI/SpecRenderer.pm
+++ lib/Mojolicious/Plugin/OpenAPI/SpecRenderer.pm
@@ -85,6 +85,10 @@ sub _render_spec {
   local $spec->{basePath} = $c->url_for($spec->{basePath});
   local $spec->{host}     = $c->req->url->to_abs->host_port;
 
+  if ($self->validator->version ge '3') {
+    delete $spec->{$_} for qw/basePath host/;
+  }
+
   return $c->render(json => $spec) unless $format eq 'html';
   return $c->render(
     handler   => 'ep',
