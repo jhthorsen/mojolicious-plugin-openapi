736fb9e8d987199127d16bddaa3ead3e09837e9e
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-03-08 18:39:09 +0900

    Released version 3.25
     - Switched to jsonhtmlify in SpecRenderer #167
     - SpecRenderer styling is using colors from the OpenAPI logo
     - Changed to rendering operationId, instead of method/path in headings
     - Documented demo page and template structure for SpecRenderer
     - Moved "Base URL", "Contact" and "License" in SpecRenderer

--- lib/Mojolicious/Plugin/OpenAPI.pm
+++ lib/Mojolicious/Plugin/OpenAPI.pm
@@ -7,7 +7,7 @@ use Mojo::JSON;
 use Mojo::Util;
 use constant DEBUG => $ENV{MOJO_OPENAPI_DEBUG} || 0;
 
-our $VERSION = '3.24';
+our $VERSION = '3.25';
 my $X_RE = qr{^x-};
 
 has route     => sub {undef};
