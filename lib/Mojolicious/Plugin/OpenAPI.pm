7506e5c81406929b75f1efc43300ff9091983c11
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-03-31 20:43:02 +0900

    Released version 3.31
     - Fix SpecRenderer should not add ".json" to the "servers" URL #174

--- lib/Mojolicious/Plugin/OpenAPI.pm
+++ lib/Mojolicious/Plugin/OpenAPI.pm
@@ -7,7 +7,7 @@ use Mojo::JSON;
 use Mojo::Util;
 use constant DEBUG => $ENV{MOJO_OPENAPI_DEBUG} || 0;
 
-our $VERSION = '3.30';
+our $VERSION = '3.31';
 my $X_RE = qr{^x-};
 
 has route     => sub {undef};
