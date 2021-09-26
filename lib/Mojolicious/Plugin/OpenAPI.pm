74b9128a8065dc22e445de3d3254cf718b12b78c
Jan Henning Thorsen <jhthorsen@cpan.org>
2021-09-26 18:53:25 +0900

    Building against development version of J::V for now

--- lib/Mojolicious/Plugin/OpenAPI.pm
+++ lib/Mojolicious/Plugin/OpenAPI.pm
@@ -8,7 +8,7 @@ use Mojolicious::Plugin::OpenAPI::Parameters;
 
 use constant DEBUG => $ENV{MOJO_OPENAPI_DEBUG} || 0;
 
-our $VERSION = '4.06';
+our $VERSION = '4.99_01';
 
 has route     => sub {undef};
 has validator => sub { JSON::Validator::Schema->new; };
